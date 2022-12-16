a6fb3a0a0bb5f95dfaa9ef2be9ef7e708f6855f9
johnnyonline <johnnyonline.eth@gmail.com>
2022-12-16 15:20:06 +0200

    Add ArbiRegistry

--- script/arbitrum/InitFortressArbi.s.sol
+++ script/arbitrum/InitFortressArbi.s.sol
@@ -3,10 +3,12 @@ pragma solidity 0.8.8;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-import "forge-std/Script.sol";
-import "forge-std/console.sol";
+// import "forge-std/Script.sol";
+import "lib/forge-std/src/Script.sol";
+// import "forge-std/console.sol";
+import "lib/forge-std/src/console.sol";
 
-import "src/mainnet/utils/FortressRegistry.sol";
+import "src/arbitrum/utils/FortressArbiRegistry.sol";
 import "script/arbitrum/utils/compounders/gmx/InitGlpCompounder.sol";
 
 contract InitFortress is Script, InitGlpCompounder {
@@ -20,16 +22,20 @@ contract InitFortress is Script, InitGlpCompounder {
         vm.startBroadcast(deployerPrivateKey);
 
         // FortressSwap fortressSwap = new FortressSwap(address(owner));
-        // FortressRegistry fortressRegistry = new FortressRegistry();
+        FortressArbiRegistry _fortressArbiRegistry = new FortressArbiRegistry(address(owner));
         
         // initialize GLP Compounder
-        address _glpCompounder = _initializeGlpCompounder(address(owner), address(platform));
+        address _glpCompounder = _initializeGlpCompounder(address(owner), address(platform), address(_fortressArbiRegistry));
 
         console.log("GlpCompounder address: ", address(_glpCompounder));
 
         string memory path = "script/arbitrum/utils/glp-compounder.txt";
         // string memory data = string(abi.encodePacked("!", "FortressRegistry=", string(vm.toString(address(fortressRegistry))), "!" ));
         string memory data = string(abi.encodePacked(string(vm.toString(address(_glpCompounder)))));
+        vm.writeFile(path, data);
+
+        path = "script/arbitrum/utils/arbi-registry.txt";
+        data = string(abi.encodePacked(string(vm.toString(address(_fortressArbiRegistry)))));
         vm.writeFile(path, data);
 
         vm.stopBroadcast();
--- script/arbitrum/utils/compounders/gmx/InitGlpCompounder.sol
+++ script/arbitrum/utils/compounders/gmx/InitGlpCompounder.sol
@@ -3,11 +3,18 @@ pragma solidity 0.8.8;
 
 import "src/arbitrum/compounders/gmx/GlpCompounder.sol";
 
-contract InitGlpCompounder {
+import "script/arbitrum/utils/InitBase.sol";
+
+contract InitGlpCompounder is InitBase {
     
-    function _initializeGlpCompounder(address _owner, address _platform) public returns (address) {
+    function _initializeGlpCompounder(address _owner, address _platform, address _registry) public returns (address) {
+
+        /// @notice The address of sGLP token.
+        address sGLP = 0x5402B5F40310bDED796c7D0F3FF6683f5C0cFfdf;
 
         GlpCompounder _glpCompounder = new GlpCompounder(_owner, _platform, address(0));
+
+        FortressArbiRegistry(_registry).registerTokenCompounder(address(_glpCompounder), sGLP, "fortGLP", "Fortress GLP");
         
         return address(_glpCompounder);
     }
