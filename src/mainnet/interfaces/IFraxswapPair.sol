a94d22e48044353fcdf322ff80abd00ed7e11c88
johnnyonline <johnnyonline.eth@gmail.com>
2022-12-17 16:59:31 +0200

    Update solc version

--- src/mainnet/interfaces/IFraxswapPair.sol
+++ src/mainnet/interfaces/IFraxswapPair.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
+pragma solidity ^0.8.17;
 
 interface IUniswapV2PairV5 {
     event Approval(address indexed owner, address indexed spender, uint value);
