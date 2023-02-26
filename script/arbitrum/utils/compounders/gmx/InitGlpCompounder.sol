f2dd1a9d1cd230895ca08c522428410e69100d18
johnnyonline <johnnyonline.eth@gmail.com>
2023-02-26 23:25:27 +0200

    wip

--- script/arbitrum/utils/compounders/gmx/InitGlpCompounder.sol
+++ script/arbitrum/utils/compounders/gmx/InitGlpCompounder.sol
@@ -11,7 +11,6 @@ contract InitGlpCompounder is InitBaseArbi {
 
         _initSwapGlp(_swap);
 
-        /// @notice The address of sGLP token.
         address sGLP = 0x5402B5F40310bDED796c7D0F3FF6683f5C0cFfdf;
 
         _underlyingAssets5[0] = WETH;
@@ -21,9 +20,7 @@ contract InitGlpCompounder is InitBaseArbi {
         _underlyingAssets5[4] = USDT;
 
         GlpCompounder _glpCompounder = new GlpCompounder(cryptoDescription, _owner, _platform, _swap, _underlyingAssets5);
-        // address _glpCompounder = 0x9363e5e4a7aDfB346BEA0fe87a8BD02fddA48855;
-
-        // FortressArbiRegistry(_registry).registerTokenCompounder(address(_glpCompounder), sGLP, "fortGLP", "Fortress GLP");
+        
         YieldOptimizersRegistry(_registry).registerTokenCompounder(address(_glpCompounder), sGLP);
         
         return address(_glpCompounder);
@@ -33,18 +30,18 @@ contract InitGlpCompounder is InitBaseArbi {
 
         FortressArbiSwap _swap = FortressArbiSwap(payable(_fortressSwap));
 
-        // WETH --> LINK 
-        if (!(_swap.routeExists(WETH, LINK))) {
-            _poolType1[0] = 14;
+        // // WETH --> LINK 
+        // if (!(_swap.routeExists(WETH, LINK))) {
+        //     _poolType1[0] = 14;
             
-            _poolAddress1[0] = address(0);
+        //     _poolAddress1[0] = address(0);
             
-            _fromList1[0] = WETH;
+        //     _fromList1[0] = WETH;
             
-            _toList1[0] = LINK;
+        //     _toList1[0] = LINK;
 
-            _swap.updateRoute(WETH, LINK, _poolType1, _poolAddress1, _fromList1, _toList1);
-        }
+        //     _swap.updateRoute(WETH, LINK, _poolType1, _poolAddress1, _fromList1, _toList1);
+        // }
 
         // WETH --> FRAX
         if (!(_swap.routeExists(WETH, FRAX))) {
@@ -85,43 +82,43 @@ contract InitGlpCompounder is InitBaseArbi {
             _swap.updateRoute(WETH, USDT, _poolType1, _poolAddress1, _fromList1, _toList1);
         }
 
-        // WETH --> WBTC
-        if (!(_swap.routeExists(WETH, WBTC))) {
-            _poolType1[0] = 14;
+        // // WETH --> WBTC
+        // if (!(_swap.routeExists(WETH, WBTC))) {
+        //     _poolType1[0] = 14;
             
-            _poolAddress1[0] = address(0);
+        //     _poolAddress1[0] = address(0);
             
-            _fromList1[0] = WETH;
+        //     _fromList1[0] = WETH;
             
-            _toList1[0] = WBTC;
+        //     _toList1[0] = WBTC;
 
-            _swap.updateRoute(WETH, WBTC, _poolType1, _poolAddress1, _fromList1, _toList1);
-        }
+        //     _swap.updateRoute(WETH, WBTC, _poolType1, _poolAddress1, _fromList1, _toList1);
+        // }
 
-        // WETH --> wstETH
-        if (!(_swap.routeExists(WETH, WSTETH))) {
-            _poolType1[0] = 12;
+        // // WETH --> wstETH
+        // if (!(_swap.routeExists(WETH, WSTETH))) {
+        //     _poolType1[0] = 12;
             
-            _poolAddress1[0] = BALANCER_WSTETHWETH;
+        //     _poolAddress1[0] = BALANCER_WSTETHWETH;
             
-            _fromList1[0] = WETH;
+        //     _fromList1[0] = WETH;
             
-            _toList1[0] = WSTETH;
+        //     _toList1[0] = WSTETH;
 
-            _swap.updateRoute(WETH, WSTETH, _poolType1, _poolAddress1, _fromList1, _toList1);
-        }
+        //     _swap.updateRoute(WETH, WSTETH, _poolType1, _poolAddress1, _fromList1, _toList1);
+        // }
 
-        // wstETH --> WETH
-        if (!(_swap.routeExists(WSTETH, WETH))) {
-            _poolType1[0] = 12;
+        // // wstETH --> WETH
+        // if (!(_swap.routeExists(WSTETH, WETH))) {
+        //     _poolType1[0] = 12;
             
-            _poolAddress1[0] = BALANCER_WSTETHWETH;
+        //     _poolAddress1[0] = BALANCER_WSTETHWETH;
             
-            _fromList1[0] = WSTETH;
+        //     _fromList1[0] = WSTETH;
             
-            _toList1[0] = WETH;
+        //     _toList1[0] = WETH;
 
-            _swap.updateRoute(WSTETH, WETH, _poolType1, _poolAddress1, _fromList1, _toList1);
-        }
+        //     _swap.updateRoute(WSTETH, WETH, _poolType1, _poolAddress1, _fromList1, _toList1);
+        // }
     }
 }
